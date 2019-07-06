f708b3cc77b3dc0142790901842327840708c397
Yusra Wasi <wasiyusra@gmail.com>
2019-07-06 19:21:50 +0500

    InventoryController-edit

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -204,9 +204,22 @@ class InventoryController extends Controller
     public function edit($id)
     {
         //
-        $products = Product::all();
-        $inventory_item = Inventory::find($id);
-        return view('inventory.edit')->with('inventory_item',$inventory_item)->with('products',$products);;
+        // $products = Product::all();
+        // $inventory_item = Inventory::find($id);
+        // return view('inventory.edit')->with('inventory_item',$inventory_item)->with('products',$products);
+
+        $inventory = Inventory::find($id);
+
+      
+        return [
+            'id' => $inventory->id,
+            'quantity' => $inventory->quantity,
+            'price' => $inventory->price,
+            'batch_code' => $inventory->batch_code,
+            'is_expirable' => $inventory->is_expirable,
+            'expiry_date' => $inventory->expiry_date,
+        ];
+        
     }
 
     /**
